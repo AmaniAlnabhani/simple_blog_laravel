fb2e3d33fee39d82f6f927d4a24f95452864ef8e
AmaniALnabhani <amanialnabhani7@gmail.com>
2024-01-20 09:52:53 +0400

    template inheritance

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,33 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Blog</a> 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/posts">All Post</a>
-        </li>
-        </ul>
-    </div>  
+@extends('layouts.app')
 
-    </div>
-  </div>
-</nav>
-<div class="container mt-5">
-    <div class="text-center">
-    <button type="button" class="btn btn-success">Create Post</button>
-
-    </div>
-    
-    <table class="table mt-4">
+@section('title') Index
+@endsection
+@section('content')
   <thead>
     <tr>
       <th scope="col">#</th>
@@ -57,7 +32,4 @@
    
   </tbody>
 </table>
-</div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
