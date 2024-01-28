f338cbefbe41504442435261aa2ad115a0700df9
AmaniALnabhani <amanialnabhani7@gmail.com>
2024-01-28 19:51:53 +0400

    migration

---
+++ database/migrations/2024_01_28_154256_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            //varchar  title
+            $table->string('title');
+            //text description
+            $table->text('description');
+            $table->timestamps(); /// created at and update at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
